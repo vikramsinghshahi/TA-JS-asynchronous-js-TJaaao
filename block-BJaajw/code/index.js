66867519a4b62003b1b0e357de6b45ef93f4228d
Vikram Singh Shahi <vikramsinghshahi@gmail.com>
2021-08-20 11:48:44 +0530

    update

--- block-BJaajw/code/index.js
+++ block-BJaajw/code/index.js
@@ -1,78 +1,118 @@
-let url = `https://api.spaceflightnewsapi.net/v3/articles?_limit=30`;
+(function () {
+  let url = `https://api.spaceflightnewsapi.net/v3/articles?_limit=30`;
 
-let newsELm = document.querySelector('.news');
+  let newsELm = document.querySelector('.news');
 
-let selectElm = document.querySelector('select');
+  let selectElm = document.querySelector('select');
 
-let allNews = [];
+  let main = document.querySelector('.main');
 
-function displayOptions(sources) {
-  sources.forEach((source) => {
-    let option = document.createElement('option');
-    option.innerText = source;
-    option.value = source;
-    selectElm.append(option);
-  });
-}
+  let errorMessage = document.querySelector('.error-message');
 
-function renderNews(news) {
-  newsELm.innerHTML = '';
-  news.forEach((newsItem) => {
-    let li = document.createElement('li');
-    let img = document.createElement('img');
-    img.src = newsItem.imageUrl;
+  let allNews = [];
 
-    let div = document.createElement('div');
+  /* <div class="donut"></div> */
 
-    let span = document.createElement('span');
-    span.innerText = newsItem.newsSite;
-
-    let h3 = document.createElement('h3');
-    h3.innerText = newsItem.title;
-
-    let a = document.createElement('a');
-
-    let button = document.createElement('button');
-
-    button.innerText = 'Read More';
-    a.href = newsItem.url;
-
-    a.append(button);
-
-    div.append(span, h3, a);
-    li.append(img, div);
-    newsELm.append(li);
-  });
-}
-
-fetch(url)
-  .then((res) => {
-    // console.log(res);
-    // console.log(res.json());
-    return res.json();
-  })
-  .then((news) => {
-    console.log(news);
-    let allSources = Array.from(
-      new Set(
-        news.map((site) => {
-          return site.newsSite;
-        })
-      )
-    );
-    allNews = news;
-    renderNews(news);
-    displayOptions(allSources);
-  });
-
-selectElm.addEventListener('change', (event) => {
-  let source = event.target.value.trim();
-  console.log(source);
-
-  if (source) {
-    let filterNews = allNews.filter((news) => news.newsSite === source);
-    renderNews(filterNews);
-  } else {
-    renderNews(allNews);
+  function handleError(msg = 'Something went wrong') {
+    main.style.display = 'none';
+    errorMessage.innerText = msg;
+    errorMessage.style.display = 'block';
   }
-});
+
+  function handleSpinner(status = false) {
+    if (status) {
+      newsELm.innerHTML = `<div class="spinner"><div class="donut"></div><div>`;
+    }
+  }
+
+  function displayOptions(sources) {
+    sources.forEach((source) => {
+      let option = document.createElement('option');
+      option.innerText = source;
+      option.value = source;
+      selectElm.append(option);
+    });
+  }
+
+  function renderNews(news) {
+    newsELm.innerHTML = '';
+    news.forEach((newsItem) => {
+      let li = document.createElement('li');
+      let img = document.createElement('img');
+      img.src = newsItem.imageUrl;
+
+      let div = document.createElement('div');
+
+      let span = document.createElement('span');
+      span.innerText = newsItem.newsSite;
+
+      let h3 = document.createElement('h3');
+      h3.innerText = newsItem.title;
+
+      let a = document.createElement('a');
+
+      let button = document.createElement('button');
+
+      button.innerText = 'Read More';
+      a.href = newsItem.url;
+
+      a.append(button);
+
+      div.append(span, h3, a);
+      li.append(img, div);
+      newsELm.append(li);
+    });
+  }
+
+  function init() {
+    handleSpinner(true);
+    fetch(url)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
+        } else {
+          throw new Error('Response not ok!');
+        }
+        // console.log(res);
+        // console.log(res.json());
+      })
+      .then((news) => {
+        handleSpinner(true);
+        console.log(news);
+        let allSources = Array.from(
+          new Set(
+            news.map((site) => {
+              return site.newsSite;
+            })
+          )
+        );
+        allNews = news;
+        renderNews(news);
+        displayOptions(allSources);
+      })
+      .catch((error) => {
+        handleError(error);
+      })
+      .finally(() => {
+        handleSpinner();
+      });
+  }
+
+  selectElm.addEventListener('change', (event) => {
+    let source = event.target.value.trim();
+    console.log(source);
+
+    if (source) {
+      let filterNews = allNews.filter((news) => news.newsSite === source);
+      renderNews(filterNews);
+    } else {
+      renderNews(allNews);
+    }
+  });
+
+  if (navigator.onLine) {
+    init();
+  } else {
+    handleError('Check your internet connection');
+  }
+})();
